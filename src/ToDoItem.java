cc5944009c4295a25e06df3130b8b77433af71bc
Matthew <Matthew@outlook.com.au>
2024-03-04 10:36:41 +1000

    added gettersetter

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -4,7 +4,16 @@ public class ToDoItem {
     private boolean isDone;
 
     public void TodoItem(String description){
+        this.description = description;
+        isDone = false;
+    }
 
+    //Getter
+    public String getDescription(){
+        return description;
+    }
 
+    public String setDescription(String description){
+         return this.description = description;
     }
 }
