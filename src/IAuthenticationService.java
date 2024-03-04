90ef516e71cbef6a54eaed45ac929439f1eb5a9c
Matthew <Matthew@outlook.com.au>
2024-03-04 10:07:32 +1000

    Added ToDoItem File

--- src/IAuthenticationService.java
+++ src/IAuthenticationService.java
@@ -1,2 +1,4 @@
-public class IAuthenticationService {
+public interface IAuthenticationService {
+    User signUp(String username, String password);
+    User logIn(String username, String password);
 }
---
+++ src/ToDoItem.java
@@ -0,0 +1,5 @@
+public class ToDoItem {
+    private String description;
+
+    private boolean isDone;
+}
